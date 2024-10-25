ff7110459d4cfc07d7e3c07c6f7491199b6a4f36
dinhvandong <dongvan.ict3@gmail.com>
2024-10-25 15:56:03 +0700

    update new code
    update new code

---
+++ src/main/java/com/vti_education/shoppe_demo/ShoppeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vti_education.shoppe_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vti_education/shoppe_demo/ShoppeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti_education.shoppe_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
